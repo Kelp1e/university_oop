02505e9c92c6283c905ecd1b2f90e8459df46bd9
Vladislav Mykhailovskyi <50411145+Kelp1e@users.noreply.github.com>
2022-10-31 16:51:19 +0200

    Add files via upload

---
+++ lab_3/Book/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Book
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Can you write your book title? ");
+            string title = Console.ReadLine();
+            Console.Write("Also author : ");
+            string author = Console.ReadLine();
+            Console.WriteLine("The last that we need it's content : ");
+            string content = Console.ReadLine();
+            Book book = new Book(new Title(title), new Author(author), new Content(content));
+            Console.WriteLine();
+            Console.WriteLine("Okey, the process completed.");
+            Console.Clear();
+            book.Title.Show();
+            book.Author.Show();
+            book.Content.Show();
+            Console.WriteLine();
+        }
+
+        
+    }
+}
